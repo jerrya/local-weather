d7062fc0d5d612d6fdfe122471bc387301530c14
Alexius <jerryalex08@gmail.com>
2016-10-21 15:56:31 -0500

    HTML insertion working and retrieval of weather

--- weather.js
+++ weather.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
 
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=285cf80f33584eafeb090d3848835b3c", function(data){
-		$.each(data, function(key, value) {
-			//alert(key);
-		});
+	var temp;
+
+	// Workaround for getting coordinates through an insecure connection
+	$.getJSON("http://ip-api.com/json", function(data){
+		var lat = data.lat;
+		var long = data.lon;
+		var city = data.city;
+		getWeather(lat, long, city);
 	});
+
+	function getWeather(latitude, longitude, city) {
+		var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=285cf80f33584eafeb090d3848835b3c";
+		$.getJSON(url, 
+			function(data){
+				temp = data.main.temp;
+				var description = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+				var icon = data.weather[0].icon + ".png";
+				$("#temperature").html(convertToFahrenheit());
+				$("#description").html(description + " in " + city);
+				$("#icon").html("<img class='img img-responsive center-block' src='http://openweathermap.org/img/w/" + icon + "'>");
+		});
+	}
+
+	function convertToCelsius() {
+		return (temp - 273.15).toFixed(1) + "° C";
+	}
+
+	function convertToFahrenheit() {
+		return ( (temp * 9/5) - 459.67).toFixed(1) + "° F";
+	}
 
 });
